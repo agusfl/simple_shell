397b2a7da2ecb1c64c0006fbf43aca71f69f5910
JuanManuelReyes <manu_reyes2003@hotmail.com>
2022-03-31 11:43:40 -0300

    Handler

---
+++ shell.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/*Global Scope Variable*/
+int signal;
+
+
+/**
+ * handler -  
+ * Return: 
+ **/
+void signal_catcher (signal)
+{
+    if (signal == 0)
+        algo;
+    else
+        otracosa;
+}
+
