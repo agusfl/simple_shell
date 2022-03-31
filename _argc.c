28a8b8ec5cfa3d2281d9c87b489c25e9dafe8074
agusfl <agusflom@hotmail.com>
2022-03-31 13:39:31 -0700

    comments

--- _argc.c
+++ _argc.c
@@ -1,8 +1,16 @@
 #include <stdio.h>
 
+/**
+ * main - function to print a command line without using the argument argc
+ * @argc: argument counter (unused)
+ * @argv: arguments vector
+ * Return: number of arguments
+ */
+
 int main(int __attribute__((unused)) argc, char *argv[])
 {
 	int i;
+
 	while (argv[i] != NULL)
 	{
 		printf("%s\n", argv[i]);
