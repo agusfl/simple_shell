5706552dd833772eaee5d89416367380bbd4b1d3
agusfl <agusflom@hotmail.com>
2022-03-31 12:34:28 -0700

    implementation of function PID and PPID

---
+++ getppid.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * main - program to print the PID of the parent process.
+ * Return: 0 if success
+ */
+
+int main(void)
+{
+	pid_t proc_id, parent_pid;
+
+	proc_id = getpid();
+	parent_pid = getpid();
+
+	printf("Process ID: %d\n", proc_id);
+	printf("Parent Process ID: %d\n", parent_pid);
+
+	return (0);
+}
