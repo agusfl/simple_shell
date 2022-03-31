26f6baf4a92025c9638f19069d234f3709fbc4b8
JuanManuelReyes <juanmanuelreyesarrambide@gmail.com>
2022-03-31 16:58:59 -0700

    asd

--- _strtok.c
+++ _strtok.c
@@ -14,3 +14,24 @@ int _strtok (void)
 
 
 }
+
+/**
+ *
+ *
+ *
+ *
+ *
+ *
+ **/
+unsigned int is_delim(char c, char *delim)
+{
+	while(*delim != '\0')
+	{
+		if(c == *delim)
+			return 1;
+
+		delim++;
+	}
+
+	return 0;
+}
