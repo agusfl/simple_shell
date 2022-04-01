2fe29b4f902e431d60cac4b827b1914e00eae36e
JuanManuelReyes <juanmanuelreyesarrambide@gmail.com>
2022-04-01 04:47:37 -0700

    NewThings

--- _strtok.c
+++ _strtok.c
@@ -8,14 +8,27 @@ int main (void)
 	char str[100] = "Numbers: 1, 2, 3, 4, 5, 6, 7, 8, 9, 10.";
 	char separator[2] = ",";
 	char *token;
+	char *array[500];
+	int i = 0, length;
+
+	length = sizeof(array)/sizeof(array[0]);
 
 	token = strtok(str, separator);
 
 	while (token != NULL)
 	{
 		printf ("%s\n", token);
-		
+		array[i] = token;
 		token = strtok (NULL, separator);
+		i++;
+	}
+	printf("====================================================\n");
+	for (i = 0; i < length; i++)
+	{
+		printf("%s\n",array[i]);
+
+		if (array[i] == NULL || array[i] == 0)
+			exit;
 	}
 
 	return (0);
