188b2aed086372dd60ce2315e81db4191e8b9f49
agusfl <agusflom@hotmail.com>
2022-04-01 06:06:48 -0700

    add free and comments, and remove the new line

--- _getline.c
+++ _getline.c
@@ -7,19 +7,21 @@
 
 int main(void)
 {
-	char *buffer;
-	size_t bufsize = 32;
+	char *buffer = NULL; /* Buena practica setear en NULL */
+	size_t bufsize = 32; /* Cambiar cantidad de espacio */
 
 	buffer = malloc(bufsize * sizeof(char));
 	if (buffer == NULL)
 	{
-		perror("Unable to allocate buffer");
-		exit(0);
+		perror("Unable to allocate buffer"); /*Print message error */
+		free(buffer); /*free buffer in case it fails malloc */
+		exit(0); /*exit function in case of error */
 	}
 
 	printf("$ ");
 	getline(&buffer, &bufsize, stdin);
-	printf("%s\n", buffer);
+	printf("%s", buffer);
+	free(buffer);
 
 	return (0);
 }
