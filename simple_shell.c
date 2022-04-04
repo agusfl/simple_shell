7596d2d5aa362ef160f94a13b6636e9f11383fee
agusfl <agusflom@hotmail.com>
2022-04-04 07:30:30 -0700

    simple shell that prints a command

--- simple_shell.c
+++ simple_shell.c
@@ -7,6 +7,7 @@
 
 int main(int __attribute__((unused)) argc, char **argv)
 {
+	/*getline*/
 	char *buffer = NULL;/* Buena practica setear en NULL*/
 	size_t bufsize = 1024;/*Por defecto la funcion getline realoca al tamanio que se necesite*/
 
@@ -19,10 +20,55 @@ int main(int __attribute__((unused)) argc, char **argv)
 	}
 	printf("$ ");
 	getline(&buffer, &bufsize, stdin);
-	printf("%s", buffer);
 
+	/*Spaces*/
+
+	char *str;
+        int space = 0, x = 0;
+
+        str = buffer;
+
+        while (str[x] != '\0')
+        {
+                if (str[x] == 32)
+                {
+                        space++;
+                }
+                x++;
+        }
+        space++;
+
+	/*strtok*/
+
+	char *str2, *token;
+        char *separator = " ";
+        int i = 0, size;
+
+        str2 = buffer;
+        size = space;
+
+        char **token_array = malloc(sizeof(char *) * size);
+	
+	token = strtok(str2, "\n");
+        token = strtok(str2, separator);
+
+        while (token != NULL)
+        {
+                token_array[i] = _strdup(token);
+                if (token_array == NULL)
+                {
+                        _free(2, token_array);
+                        return (0);
+                }
+		token = strtok(NULL, separator);
+                i++;
+        }
+        token_array[i] = token; /* Token pasa a ser NULL porque salio del while en NULL y se lo asignamos al ultimo
+                                   elemento de nuestro array para que termine en NULL */
+
+	/*shell*/
 	pid_t child;
-	char *argV[] = {buffer, NULL};
+
 	char *env[] = {NULL};
 	int status, child_count = 0;
 
@@ -36,9 +82,7 @@ int main(int __attribute__((unused)) argc, char **argv)
 		}
 		if (child == 0) /*if it is 0 means that is the child process */
 		{
-			printf("Child process ID: %d\n", getpid());
-
-			if (execve(argV[0], argV, env) == -1)
+			if (execve(token_array[0], token_array, env) == -1)
 			{
 				perror("Could not execute execve");
 			}
@@ -46,7 +90,6 @@ int main(int __attribute__((unused)) argc, char **argv)
 		else
 		{
 			wait(&status);
-			printf("This is the parent process ID: %d\n", getppid());
 		}
 	child_count++;
 	}
