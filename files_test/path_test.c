78dee235517e146410ca4560178347994cb0d186
JuanManuelReyes <manu_reyes2003@hotmail.com>
2022-04-06 19:49:32 -0300

    asd

---
+++ files_test/path_test.c
@@ -0,0 +1,132 @@
+#include "test.h"
+
+int maing()
+{
+
+}
+
+/**
+ * _getpath - Search the PATH variable in environ
+ *Return: Returns a pointer to the position of PATH variable
+ **/
+char *_getpath()
+{
+	char *path = NULL;
+	int i = 0;
+
+	for (i = 0; environ[i] != NULL; i++)
+	{
+		if (_strcmp(environ[i], "PATH") == 0)
+		{
+			path = malloc(sizeof(char *) * (_strlen(environ[i])));
+			if (path == NULL)
+			{
+				_free(1, path);
+				exit(-1);
+			}
+			_strcpy(path, environ[i]);
+			break;
+		}
+	}
+	return (path);
+}
+
+/**
+ * strcomparer - Compares two given  strings
+ * @s1: Given S1 String
+ * @s2: Given S2 String
+ * Return: 0 if both are equals or ascii diff
+*/
+int _strcmp(char *s1, char *s2)
+{
+	int diff, index;
+
+	diff = index = 0;
+	if (s1 == NULL || s2 == NULL)
+		return (-1);
+	while (s1[index] != '\0' && s2[index] != '\0')
+	{
+		diff = s1[index] - s2[index];
+		if (diff != 0)
+			break;
+		index++;
+	}
+	return (diff);
+}
+
+/**
+ * _strlen - our own implementation of strlen function
+ * @str: string that we want to know the length
+ * Return: length of the string
+ */
+
+int _strlen(char *str)
+{
+	int count = 0;
+
+	while (str[count] != '\0')
+	{
+		count++;
+	}
+	return (count);
+}
+
+/**
+ * _free - function to free 1 pointer or a double pointer
+ * @n: indicates if it is a 1 pointer to be freed or a double pointer
+ **/
+
+void _free(int n, ...)
+{
+	char *ptr;
+	char **dptr;
+	va_list valist;
+	int i;
+
+	va_start(valist, n);
+
+	if (n == 1)
+	{
+		ptr = va_arg(valist, char *);
+		if (ptr == NULL)
+		{
+			return;
+		}
+		free(ptr);
+	}
+	if (n == 2)
+	{
+		dptr = va_arg(valist, char **);
+		if (dptr == NULL)
+		{
+			return;
+		}
+		for (i = 0; dptr[i] != NULL; i++)
+		{
+			free(dptr);
+		}
+		free(dptr);
+	}
+	va_end(valist);
+}
+
+/**
+ * _strcpy - copies the string pointed to by src
+ * @dest: copy source to this buffer
+ * @src: source to copy
+ * Return: copy of the original source
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int i, length = 0;
+
+	char *new = dest;
+
+	length = _strlen(src);
+
+	for (i = 0; i <= (length + 1); i++)
+		new[i] = src[i];
+
+	return (new);
+}
---
+++ files_test/test.h
@@ -0,0 +1,5 @@
+char *_getpath();
+int _strcmp(char *s1, char *s2);
+int _strlen(char *str);
+void _free(int n, ...);
+char *_strcpy(char *dest, char *src);
