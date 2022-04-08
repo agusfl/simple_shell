133708408c1858383d1415d701457729db197413
JuanManuelReyes <manu_reyes2003@hotmail.com>
2022-04-08 19:07:45 -0300

    asd

--- finalVersion/getters_functions.c
+++ finalVersion/getters_functions.c
@@ -39,14 +39,14 @@ char *_getline(void)
  * _getpath - Search the PATH variable in environ
  *Return: Returns a pointer to the position of PATH variable
  **/
-char *_getpath()
+char **_getpath()
 {
-	char *path = NULL;
-	int i = 0;
+	char *path = NULL, **tokenized_path;
+	int i = 0, size = 0;
 
 	for (i = 0; environ[i] != NULL; i++)
 	{
-		if (_strcmp_path(environ[i], "PATH") == 0)
+		if (_strcmp(environ[i], "PATH") == 0)
 		{
 			path = malloc(sizeof(char *) * (_strlen(environ[i])));
 			if (path == NULL)
@@ -58,5 +58,16 @@ char *_getpath()
 			break;
 		}
 	}
-	return (path);
+	path += 5;
+
+	size = _colons(path);
+	tokenized_path = _pathtok(path, size);
+
+	for (i = 0; tokenized_path[i] != NULL; i++)
+        {
+                tokenized_path[i] = _strcat(tokenized_path[i], "/");
+        }
+
+
+	return (tokenized_path);
 }
