d28c64518a55abb105353b68c9f5af9b310869f1
JuanManuelReyes <juanmanuelreyesarrambide@gmail.com>
2022-04-09 06:34:38 -0700

    test.sh

---
+++ test.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+echo "=============Teste 1============="
+echo "Command tested: "\n\nls" | ./hsh"
+echo "Correct output: ↓"
+echo -e "\n\nls" | /bin/sh
+echo "---------------------------------"
+echo "Your output: ↓"
+echo -e "\n\nls" | ./hsh
+echo "================================="
+echo ""
+echo "=============Teste 2============="
+echo "Command tested: "\t\tpwd" | ./hsh"
+echo "Correct output: ↓"
+echo -e "\t\tpwd" | /bin/sh
+echo "---------------------------------"
+echo "Your output: ↓"
+echo -e "\t\tpwd" | ./hsh
+echo "================================="
+echo ""
+echo "=============Teste 3============="
+echo "Command tested: "qwerty" | ./hsh"
+echo "Correct output: ↓"
+echo "qwerty" | /bin/sh
+echo "---------------------------------"
+echo "Your output: ↓"
+echo "qwerty" | ./hsh
+echo "================================="
+echo ""
+echo "=============Teste 4============="
+echo "Command tested: "qwerty" | ./hsh"
+echo "Correct output: ↓"
+echo "qwerty" | /bin/sh
+echo "---------------------------------"
+echo "Your output: ↓"
+echo "qwerty" | ./hsh
+echo "================================="
+echo ""
+echo "=============Teste 5============="
+echo "Command tested: "qwerty" | ./hsh"
+echo "Correct output: ↓"
+echo "qwerty" | /bin/sh
+echo "---------------------------------"
+echo "Your output: ↓"
+echo "qwerty" | ./hsh
+echo "================================="
