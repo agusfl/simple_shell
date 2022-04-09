28f1dca080d4ab3cb03cd6a26e8160377e3ebba8
JuanManuelReyes <manu_reyes2003@hotmail.com>
2022-04-09 13:42:19 -0300

    asd

--- NoSeQueHago/shell.c
+++ NoSeQueHago/shell.c
@@ -1,24 +1,24 @@
 #include "main.h"
 
+/**
+ * main - shell implementation
+ * Return: 0 if success
+ */
+
 int main(void)
 {
-	char *input = NULL, *realpath = NULL,  *envv = "env", **path = NULL, **tokenized_input = NULL;
+	char *input = NULL, /**realpath = NULL,*/  *envv = "env", **path = NULL, **tokenized_input = NULL;
 	int space = 0, status = 0;
 	pid_t child;
-	char *env[] = {NULL};
 
-	while (1) 
+	while (1)
 	{
-		signal(SIGINT, _ctrl_c); 
-		if (isatty(STDIN_FILENO) == 1) 
-			write(1, "$ ", 2); 
-
+		signal(SIGINT, _ctrl_c);
+		if (isatty(STDIN_FILENO) == 1)
+			write(1, "$ ", 2);
 		input = _getline();
 		space = spaces(input);
 		input = convert_tab_space(input);
-	        /*path = _getpath();*/
-		/*tokenized_input = _strtok(input, space);*/
-
 		if (ign_spaces_break_tab(input) != 1)
 		{
 			_free(1, input);
@@ -30,14 +30,32 @@ int main(void)
 			_free(1, input);
 			continue;
 		}
-
 		path = _getpath();
-                tokenized_input = _strtok(input, space);
-
-
+		tokenized_input = _strtok(input, space);
 		if (_isletter(input[0]) == 1)
 		{
-			realpath = _realpath(path, tokenized_input[0]);
+			input = _realpath(path, tokenized_input[0]);
+			child = fork();
+			if (child == -1)
+			{
+				perror("Error while creating a child process");
+				exit(4);
+			}
+			if (child == 0) /*if it is 0 means that is the child process */
+			{
+				if (execve(input, tokenized_input, environ) == -1)
+				{
+					perror(NULL); /*Con esto ya devuelve el mensaje por default*/
+					_free(1, input), _free(1, path), _free(1, tokenized_input);
+					return (0);
+				}
+			}
+			else /* parent process - waits for the child process to finish */
+				wait(&status);
+		_free(1, input), _free(1, path), _free(1, tokenized_input);
+		}
+		else
+		{
 
 			child = fork();
 			if (child == -1)
@@ -47,63 +65,40 @@ int main(void)
 			}
 			if (child == 0) /*if it is 0 means that is the child process */
 			{
-				if (execve(realpath, tokenized_input, environ) == -1)
+				if (execve(input, tokenized_input, environ) == -1)
 				{
 					perror(NULL); /*Con esto ya devuelve el mensaje por default*/
-					/*free*/
+					_free(1, input), _free(1, path), _free(1, tokenized_input);
 					return (0);
 				}
 			}
 			else /* parent process - waits for the child process to finish */
-				wait(&status);	
+				wait(&status);
 		}
-		else
-		{
-			 realpath = input;
-
-                        child = fork();
-                        if (child == -1)
-                        {
-                                perror("Error while creating a child process");
-                                exit(4);
-                        }
-                        if (child == 0) /*if it is 0 means that is the child process */
-                        {
-                                if (execve(realpath, tokenized_input, environ) == -1)
-                                {
-                                        perror(NULL); /*Con esto ya devuelve el mensaje por default*/
-                                        /*free*/
-                                        return (0);
-                                }
-                        }
-                        else /* parent process - waits for the child process to finish */
-                                wait(&status);
-		}
-		 
-		
-		
 	}
 	return (0);
 }
 
 /**
- * _realpath - asd
- * @tokenized_input: asd
- * @command: asd
- * Return: asd
+ * _realpath - function to search path of command
+ * @path: tokenaized path
+ * @command: command to search
+ * Return: string with the path of the command
  **/
+
 char *_realpath(char **path, char *command)
 {
-    char *realpath = NULL;
-    struct stat st;
+	char *realpath = NULL;
+	struct stat st;
 	int i = 0;
 
 	for (i = 0; path[i] != NULL; i++)
 	{
 		realpath = _strcat(path[i], command);
+
 		if (stat(realpath, &st) == 0)
 		{
-			return (realpath); 
+			return (realpath);
 		}
 	}
 
@@ -125,7 +120,7 @@ int print_env(void)
 	/*environ is the global variable and was declared in the main.h*/
 	{
 		write(STDOUT_FILENO, environ[i], _strlen(environ[i]));
-		/*print in standard outpu the environ variable*/
+		/*print in standard output the environ variable*/
 		write(STDOUT_FILENO, "\n", 1);
 		i++;
 	}
