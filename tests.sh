1f395aab9a6e43b09c42f9b955a4f4f751e27b3f
JuanManuelReyes <juanmanuelreyesarrambide@gmail.com>
2022-04-10 12:27:34 -0700

    TestScript

---
+++ tests.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+echo ""
+echo "=============Teste 1============="
+echo "Command tested: "ls -l" | ./hsh"
+echo "---------------------------------"
+echo "Correct output: ↓"
+echo "ls -l" | /bin/sh
+echo "---------------------------------"
+echo "Your output: ↓"
+echo "ls -l" | ./hsh
+echo "================================="
+echo ""
+echo "=============Teste 2============="
+echo "Command tested: "pwd" | ./hsh"
+echo "---------------------------------"
+echo "Correct output: ↓"
+echo "pwd" | /bin/sh
+echo "---------------------------------"
+echo "Your output: ↓"
+echo "pwd" | ./hsh
+echo "================================="
+echo ""
+echo "=============Teste 3============="
+echo "Command tested: "alfajor" | ./hsh"
+echo "---------------------------------"
+echo "Correct output: ↓"
+echo "alfajor" | /bin/sh
+echo "---------------------------------"
+echo "Your output: ↓"
+echo "alfajor" | ./hsh
+echo "================================="
+echo ""
+echo "=============Teste 4============="
+echo "Command tested: "w" | ./hsh"
+echo "---------------------------------"
+echo "Correct output: ↓"
+echo "w" | /bin/sh
+echo "---------------------------------"
+echo "Your output: ↓"
+echo "w" | ./hsh
+echo "================================="
+echo ""
+echo "=============Teste 5============="
+echo "Command tested: "echo "Hello World"" | ./hsh"
+echo "---------------------------------"
+echo "Correct output: ↓"
+echo "echo "Hello World"" | /bin/sh
+echo "---------------------------------"
+echo "Your output: ↓"
+echo "echo "Hello World"" | ./hsh
+echo "================================="
+echo ""
