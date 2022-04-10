ec3a10a90211765b2d82f2a7bb4a8abe0d351122
JuanManuelReyes <manu_reyes2003@hotmail.com>
2022-04-10 11:45:43 -0300

    Files Moved

---
+++ shell.c
@@ -0,0 +1,92 @@
+#include "main.h"
+
+/**
+ * main - shell implementation
+ * Return: 0 if success
+ **/
+
+int main(void)
+{
+	char *input = NULL, *envv = "env", **path = NULL, **tokenized_input = NULL;
+	int space = 0, status = 0;
+	pid_t child;
+
+	while (1)
+	{
+		signal(SIGINT, _ctrl_c);
+		if (isatty(STDIN_FILENO) == 1)
+			write(1, "$ ", 2);
+		input = _getline();
+		space = spaces(input);
+		input = convert_tab_space(input);
+		if (ign_spaces_break_tab(input) != 1)
+		{
+			free(input);
+			continue;
+		}
+		if (_strcmp(input, envv) == 0)
+		{
+			print_env();
+			free(input);
+			continue;
+		}
+
+		path = _getpath();
+		tokenized_input = _strtok(input, space);
+
+		if (_isletter(tokenized_input[0][0]) == 1)
+		{
+			tokenized_input[0] = _realpath(path, tokenized_input[0]);
+			child = fork();
+			if (child == -1)
+			{
+				perror(NULL);
+				_free(2, path), _free(2, tokenized_input);
+				exit(4);
+			}
+			if (child == 0) /*if it is 0 means that is the child process */
+			{
+				if (execve(tokenized_input[0], tokenized_input, environ) == -1)
+				{
+					perror(NULL); /*Con esto ya devuelve el mensaje por default*/
+					/*_free(2, path), _free(2, tokenized_input);
+					return (0);*/
+					break;
+				}
+			}
+			else /* parent process - waits for the child process to finish */
+			{
+                                wait(&status);
+                                _free_path(2, path), _free(2, tokenized_input), free(input);
+                        }
+                }
+		else
+		{
+
+			child = fork();
+			if (child == -1)
+			{
+				perror("Error while creating a child process");
+				exit(4);
+			}
+			if (child == 0) /*if it is 0 means that is the child process */
+			{
+				if (execve(tokenized_input[0], tokenized_input, environ) == -1)
+				{
+					perror(NULL); /*Con esto ya devuelve el mensaje por default*/
+					/*_free(2, path), _free(2, tokenized_input);
+					return (0);*/
+					break;
+				}
+			}
+			else /* parent process - waits for the child process to finish */
+			{
+				wait(&status);
+				_free_path(2, path), _free(2, tokenized_input), free(input);
+			}
+		}
+	}
+	 _free_path(2, path), _free(2, tokenized_input), free(input);
+	return (0);
+}
+
---
+++ tokenizer_functions.c
@@ -0,0 +1,78 @@
+#include "main.h"
+
+/**
+ * _strtok - function for make tokens from a string
+ * @str: string to be tokenaized
+ * @size: spaces to tokenaized in the string
+ * Return: pointer to an array of the string tokens
+ **/
+
+char **_strtok(char *str, int size)
+{
+	char *token, *separator = " ", *exitt = "exit";
+	char **token_array;
+	int i = 0;
+
+	size = spaces(str);
+	token_array = _calloc(size, sizeof(char *));
+
+	token = strtok(str, "\n"); /*Tokenize with \n to remove it from the string */
+	token = strtok(str, separator);
+
+	while (token != NULL)
+	{
+		token_array[i] = token;
+		if (_strcmp(token, exitt) == 0)/*If input is "exit" free memoryandExitShell*/
+		{
+			_free(1, str), _free(2, token_array);
+			exit(1);
+		}
+		if (token_array == NULL)
+		{
+			_free(2, token_array);
+			return (NULL);
+		}
+		token = strtok(NULL, separator);
+		i++;
+	}
+	token_array[i] = NULL;
+
+	return (token_array);
+}
+
+/**
+ * _pathtok - function for make tokens from a string
+ * @str: string to be tokenaized
+ * @size: spaces to tokenaized in the string
+ * Return: pointer to an array of the string tokens
+ **/
+
+char **_pathtok(char *str, int size)
+{
+	char *token, *separator = ":";
+	char **tokenized_path;
+	int i = 0;
+
+	size = _colons(str);
+	tokenized_path = _calloc(size, sizeof(char *));
+
+	token = strtok(str, "\n"); /*Tokenize with \n to remove it from the string */
+	token = strtok(str, separator);
+
+	while (token != NULL)
+	{
+		tokenized_path[i] = token;
+
+		if (tokenized_path == NULL)
+		{
+			_free(1, tokenized_path);
+			return (NULL);
+		}
+		token = strtok(NULL, separator);
+		i++;
+	}
+	tokenized_path[i] = NULL;
+
+	return (tokenized_path);
+}
+
