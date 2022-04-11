0ecd36847db76758a9faac820d13a3c950af210c
JuanManuelReyes <juanmanuelreyesarrambide@gmail.com>
2022-04-11 12:21:26 -0700

    perror

--- execution_function.c
+++ execution_function.c
@@ -19,13 +19,13 @@ void _execute_command(char **path, char **tokenized_input)
 		child = fork();
 		if (child == -1)
 		{
-			perror(NULL); /*Null return default message*/
+			perror(tokenized_input[0]); /*Null return default message*/
 		}
 		if (child == 0) /*if it is 0 means that is the child process */
 		{
 			if (execve(tokenized_input[0], tokenized_input, environ) == -1)
 			{
-				perror(NULL); /*Null return default message*/
+				perror(tokenized_input[0]); /*Null return default message*/
 
 				return;
 			}
@@ -36,7 +36,7 @@ void _execute_command(char **path, char **tokenized_input)
 		}
 	}
 	else
-		perror(NULL);
+		perror(tokenized_input[0]);
 }
 
 /**
@@ -56,14 +56,14 @@ void _execute_path(char **tokenized_input)
 		child = fork();
 		if (child == -1)
 		{
-			perror(NULL); /*Null return default message*/
+			perror(tokenized_input[0]); /*Null return default message*/
 			exit(-1);
 		}
 		if (child == 0) /*if it is 0 means that is the child process */
 		{
 			if (execve(tokenized_input[0], tokenized_input, environ) == -1)
 			{
-				perror(NULL); /*Null return default message*/
+				perror(tokenized_input[0]); /*Null return default message*/
 				return;
 			}
 		}
--- getters_function.c
+++ getters_function.c
@@ -46,14 +46,14 @@ char **_getpath()
 char *_getline(void)
 {
 	char *buffer = NULL;
-	size_t bufsize = 100; /* if we set the value on 0 there are leaks */
+	size_t bufsize = 0; /* if we set the value on 0 there are leaks */
 
-	buffer = malloc(bufsize * sizeof(char));
-	if (buffer == NULL)
+	/*buffer = malloc(bufsize * sizeof(char));*/
+	/*if (buffer == NULL)
 	{
 		free(buffer);
 		exit(0);
-	}
+	}*/
 	if (getline(&buffer, &bufsize, stdin) == -1)
 	{
 		if (isatty(STDIN_FILENO) == 1)/*if it is interactive mode make a break line*/
