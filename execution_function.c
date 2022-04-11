626a737f3ac07af88e240d907cb435772f7f1ba8
JuanManuelReyes <manu_reyes2003@hotmail.com>
2022-04-10 21:47:43 -0300

    Spaces

--- execution_function.c
+++ execution_function.c
@@ -2,66 +2,66 @@
 
 void _execute_command(char **path, char **tokenized_input)
 {
-    pid_t child;
-    int status = 0;
-    struct stat st;
+	pid_t child;
+	int status = 0;
+	struct stat st;
 
-    tokenized_input[0] = _realpath(path, tokenized_input[0]);
-    if (stat(tokenized_input[0], &st) == 0)
-    {
-        child = fork();
-        if (child == -1)
-        {
-            /*_free_path(2, path), _free(2, tokenized_input);*/
-            perror(NULL); /*Null return default message*/
-        }
-        if (child == 0) /*if it is 0 means that is the child process */
-        {
-            if (execve(tokenized_input[0], tokenized_input, environ) == -1)
-            {
-                perror(NULL); /*Null return default message*/
-                return;
-            }
-        }
-        else /* parent process - waits for the child process to finish */
-        {
-            wait(&status);
-            /*_free_path(2, path), _free(2, tokenized_input);*/
-        }
-    }
-    else
-        perror(NULL);
+	tokenized_input[0] = _realpath(path, tokenized_input[0]);
+	if (stat(tokenized_input[0], &st) == 0)
+	{
+		child = fork();
+		if (child == -1)
+		{
+			/*_free_path(2, path), _free(2, tokenized_input);*/
+			perror(NULL); /*Null return default message*/
+		}
+		if (child == 0) /*if it is 0 means that is the child process */
+		{
+			if (execve(tokenized_input[0], tokenized_input, environ) == -1)
+			{
+				perror(NULL); /*Null return default message*/
+				return;
+			}
+		}
+		else /* parent process - waits for the child process to finish */
+		{
+			wait(&status);
+			/*_free_path(2, path), _free(2, tokenized_input);*/
+		}
+	}
+	else
+		perror(NULL);
 }
 
 void _execute_path(char **tokenized_input)
 {
-    pid_t child;
-    int status = 0;
-    struct stat st;
+	pid_t child;
+	int status = 0;
+	struct stat st;
 
-    if (stat(tokenized_input[0], &st) == 0)
-    {
-        child = fork();
-        if (child == -1)
-        {
-            /*_free(2, tokenized_input);*/
-            perror(NULL); /*Null return default message*/
-            exit(4);
-        }
-        if (child == 0) /*if it is 0 means that is the child process */
-        {
-            if (execve(tokenized_input[0], tokenized_input, environ) == -1)
-            {
-                perror(NULL); /*Null return default message*/
-                return;
-            }
-        }
-        else /* parent process - waits for the child process to finish */
-        {
-            wait(&status);
-            /*_free(2, tokenized_input);*/
-        }
-    }
-    else
-        perror(NULL);
+	if (stat(tokenized_input[0], &st) == 0)
+	{
+		child = fork();
+		if (child == -1)
+		{
+			/*_free(2, tokenized_input);*/
+			perror(NULL); /*Null return default message*/
+			exit(4);
+		}
+		if (child == 0) /*if it is 0 means that is the child process */
+		{
+			if (execve(tokenized_input[0], tokenized_input, environ) == -1)
+			{
+				perror(NULL); /*Null return default message*/
+				return;
+			}
+		}
+		else /* parent process - waits for the child process to finish */
+		{
+			wait(&status);
+			/*_free(2, tokenized_input);*/
+		}
+	}
+	else
+		perror(NULL);
 }
